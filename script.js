7eb19c1c81ed1b91123de3031033e3f1c3da9958
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-16 18:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	get species(){
+		return this.species;
+	}
+	makeSound(){
+		return `The ${this.species} makes a sound`
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
